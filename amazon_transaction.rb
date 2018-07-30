3f14155ba799a6f31ef3d4f73d005f957d91ae7e
madisonparker <christaandmadison@gmail.com>
2018-07-30 14:32:06 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -2,6 +2,6 @@ class amazon
   def initialize(product, amazon_prime, print_receipt)
     @product = []
     @amazon_prime = []
-    @print_receipt = print_receipt
+    @print_receipt = []
     
 end
